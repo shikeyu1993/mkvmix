75741ad3e8eccebd711400a98d8c0d26ce9d1cc9
keyu.shi <sky19930103@sina.com>
2022-01-01 19:05:09 +0800

    Initial commit

---
+++ toollog.py
@@ -0,0 +1,19 @@
+import logging
+import os
+TAG = 'mkvmix'
+TOOLLOGPATH = f'{os.getcwd()}\\mkvmix.log'
+LOGLEVEL = logging.INFO
+
+log_level = LOGLEVEL
+logging.basicConfig(level=log_level,
+                    format='%(asctime)s %(name)-8s %(levelname)-8s %(message)s',
+                    datefmt='%m-%d %H:%M:%S',
+                    filename=TOOLLOGPATH,
+                    filemode='a')
+console = logging.StreamHandler()
+console.setLevel(log_level)
+formatter = logging.Formatter(
+    '%(asctime)s %(name)-8s %(levelname)-8s %(message)s')
+console.setFormatter(formatter)
+logging.getLogger('').addHandler(console)
+logger = logging.getLogger(TAG)
